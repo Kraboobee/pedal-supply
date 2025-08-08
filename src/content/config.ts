0175c0169b58111d976e55035040b8466d6aa76c
Kam Bux <kam.bux@aakitech.com>
2025-08-08 13:53:14 +0200

    Add accessories pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,7 +18,26 @@ const pedalsCollection = defineCollection({
     }),
 });
 
+const accessoriesCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      heroImage: image(),
+      images: z.array(
+        z.array(image()).refine((arr) => [1, 2, 3].includes(arr.length), {
+          message: "Each sub-array must contain 1, 2, or 3 items",
+        }),
+      ),
+      price: z.number().positive(),
+      order: z.number(),
+      draft: z.boolean().optional(),
+    }),
+});
+
 export const collections = {
   pedals: pedalsCollection,
+  accessories: accessoriesCollection,
 };
 
